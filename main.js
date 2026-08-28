ba562cffc78094514368c6483c8fc9a251f68eb9
BenevolenceEM <maliksofia58@gmail.com>
2026-08-27 21:05:30 -0400

    portfolio update

--- main.js
+++ main.js
@@ -1,36 +1,27 @@
-/*
-	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
-/*Modified by Benevolence Ed-Malik*/
-
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+    var $window = $(window),
+        $body = $('body'),
+        $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    // breakpoints
+    breakpoints({
+        xlarge: ['1281px', '1680px'],
+        large: ['981px', '1280px'],
+        medium: ['737px', '980px'],
+        small: [null, '736px']
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // page load animation
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
-
+    // scroll
+    $('#nav a, .scrolly').scrolly({
+        speed: 1000,
+        offset: function() { return $nav.height(); }
+    });
 })(jQuery);
